1c598517e37785077b5b924ebfe23a89d1d85aa2
Vykintas Baltrušaitis <109955939+vbaltrusaitis-reef@users.noreply.github.com>
2022-11-22 14:01:43 +0200

    Logout endpoint (#7)

--- django_firebase_auth/v0/urls.py
+++ django_firebase_auth/v0/urls.py
@@ -1,8 +1,9 @@
 from django.urls import re_path
 
-from ..views import authenticate, AdminLoginView
+from ..views import authenticate, AdminLoginView, logout
 
 urlpatterns = [
     re_path(r'firebase_authentication/v1/login/?$', authenticate),
+    re_path(r'firebase_authentication/v1/logout/?$', logout),
     re_path(r'admin/login/?$', AdminLoginView.as_view()),
 ]
